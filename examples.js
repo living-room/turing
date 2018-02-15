c25540634b9e9344ce4e9982f1c403b4a1e46387
Jonathan Dahan <hi@jonathan.is>
2018-02-15 14:21:50 -0500

    logging

--- examples.js
+++ examples.js
@@ -3,4 +3,6 @@
 const serve = require('serve')
 const path = require('path')
 const examples = path.resolve(process.cwd(), 'examples')
+console.log(process.cwd())
+console.log(examples)
 const server = serve(examples, { cors: true })
