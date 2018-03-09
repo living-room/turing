0c5a645544622107765cca13a0068eb58522f565
Jonathan Dahan <hi@jonathan.is>
2018-03-09 13:56:30 -0500

    Add a sensor for animals

---
+++ examples/sensor.js
@@ -0,0 +1,26 @@
+const Chance = require('chance')
+const chance = new Chance()
+
+const getRandomDelay = () => chance.integer({min: 1, max: 10}) * 1000
+
+const makeRandomAnimal = () => {
+   const longAnimal = chance.animal().split(' ')
+   return {
+     name: chance.first(),
+     animal: longAnimal[longAnimal.length-1],
+     x: chance.floating({min: 0, max: 1}),
+     y: chance.floating({min: 0, max: 1})
+   }
+}
+
+const printRandomAnimal = () => {
+  const {name, animal, x, y} = makeRandomAnimal()
+  console.log(`#${name} is a ${animal} at (${x}, ${y})`)
+}
+
+const printRandomAnimalRecursively = () => {
+  printRandomAnimal()
+  setTimeout(printRandomAnimalRecursively, getRandomDelay())
+}
+
+printRandomAnimalRecursively()
