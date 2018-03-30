7a1ab3f437465e431a205eb0127e310d68792f3f
Jonathan Dahan <hi@jonathan.is>
2018-03-30 19:11:56 -0400

    first update of subscribed mice

--- examples/mouse-process/mouse-process.js
+++ examples/mouse-process/mouse-process.js
@@ -21,27 +21,34 @@ socket.on('connection', socket => {
   });
 });
 
+let timer = setTimeout(()=>{}, 100)
+
 function moveMouse(vec) {
     if (lastMousePositions.length > 9) {
         lastMousePositions.pop();
         lastMousePositions.unshift(vec);
+        
+        // FIXME: really need to retract
+        /*
         room
-            .select([`$mouseLabel is a ($r, $g, $b) circle at ($x, $y) with radius $radius`])
+            .select([`$mouseLabel$num is a ($r, $g, $b) circle at ($x, $y) with radius $radius`])
             .doAll(mice => {
                 if (mice.length) {
                     setTimeout(() => {
                         // FIXME: the mouse will disappear after 100ms of inactivity!
                         // maybe this should be mice.slice(0, mice.length-1).forEach?
-                        mice.forEach(mouse => {
-                            room.retract(`${mouse.mouseLabel} is a (${mouse.r}, ${mouse.g}, ${mouse.b}) circle at (${mouse.x}, ${mouse.y}) with radius ${mouse.radius}`);
+                        room.retract(mice.map(mouse => {
+                            `${mouse.mouseLabel}${mouse.num} is a (${mouse.r}, ${mouse.g}, ${mouse.b}) circle at (${mouse.x}, ${mouse.y}) with radius ${mouse.radius}`);
                         });
                     }, 100); 
                 }
             });
-
+            */
+        
+        const alphabet = "abcdefghijklmnop"
         for (let i = 0; i < 10; i += 1) {
-            room.assert(`mouse${i} is a (${255 - i * 10}, ${255 - i * 10}, ${255 - i * 10}) circle at (${lastMousePositions[i].x}, ${lastMousePositions[i].y}) with radius ${30 - i * 2}`);
-            console.log(`mouse${i} is a (${255 - i * 10}, ${255 - i * 10}, ${255 - i * 10}) circle at (${lastMousePositions[i].x}, ${lastMousePositions[i].y}) with radius ${30 - i * 2}`);
+            room.assert(`mouse${alphabet[i]} is a (${255 - i * 10}, ${255 - i * 10}, ${255 - i * 10}) circle at (${lastMousePositions[i].x}, ${lastMousePositions[i].y}) with radius ${30 - i * 2}`);
+            console.log(`mouse${alphabet[i]} is a (${255 - i * 10}, ${255 - i * 10}, ${255 - i * 10}) circle at (${lastMousePositions[i].x}, ${lastMousePositions[i].y}) with radius ${30 - i * 2}`);
             // room.assert(`mouse${i} is a (${255 - i * 10}, ${255 - i * 10}, ${255 - i * 10}) line from (${lastMousePositions[i].x}, ${lastMousePositions[i].y}) to (${lastMousePositions[i].x + 0.2}, ${lastMousePositions[i].y + 0.2})`);
             // console.log(`mouse${i} is a (${255 - i * 10}, ${255 - i * 10}, ${255 - i * 10}) line from (${lastMousePositions[i].x}, ${lastMousePositions[i].y}) to (${lastMousePositions[i].x + 0.2}, ${lastMousePositions[i].y + 0.2})`);
         }
