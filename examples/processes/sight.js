c53e4101292a47a84ea2aae14cc5a2f5047f3918
Jonathan Dahan <hi@jonathan.is>
2018-04-12 17:06:14 -0400

    add sight again

---
+++ examples/processes/sight.js
@@ -0,0 +1,25 @@
+module.exports = room => {
+  if (!room) {
+    const Room = require('../build/room.js')
+    const room = new Room()
+  }
+
+  room.subscribe(
+    [
+      `$a is a $aspecies animal at ($ax, $ay)`,
+      `$b is a $bspecies animal at ($bx, $by)`
+    ],
+    async ({assertions, retractions}) => {
+      assertions.forEach(async ({ a, b, ax, ay, bx, by }) => {
+        if (a.word === b.word) return
+        const [dx, dy] = [100 * (ax.value - bx.value), 100 * (ay.value - by.value)]
+        const seesFact = `${a.word} sees ${b.word}`
+
+        if ((dx * dx) + (dy * dy) < (10 * 10)) {
+          room.assert(seesFact).then(console.dir)
+        } else {
+          room.retract(seesFact).then(console.dir)
+        }
+      })
+    })
+}
