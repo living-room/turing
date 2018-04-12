97cdb713eea58573ec5e15aedb601fd2f730d85b
Jonathan Dahan <hi@jonathan.is>
2018-04-12 16:31:10 -0400

    Update fear to work with animals

--- examples/processes/fear.js
+++ examples/processes/fear.js
@@ -1,37 +1,45 @@
-const Room = require('../build/room.js')
-const room = new Room()
+module.exports = room => {
+  if (!room) {
+    const Room = require('../build/room.js')
+    room = new Room()
+  }
 
-room.select(`$name is a $type animal at ($x, $y)`).then(animals => {
-  animals.forEach(animal => {
-    let { type, name } = animal
-    let fears = {
-      [type.word]: 'not',
-      mouse: 'mildly'
-    }
-    switch (type.word) {
-      case 'cat':
-        fears['dog'] = 'very'
-        fears['mouse'] = 'not'
-        break
-      case 'elephant':
-        fears['mouse'] = 'very'
-        break
-      case 'dog':
-        fears['cat'] = 'very'
-        break
-      case 'mouse':
-        fears['cat'] = 'very'
-        break
-      default:
-        break
-    }
+  const animalsWeHaveSeen = new Set()
 
-    for (var otherType in fears) {
-      let fearFact = `${type.word} ${name.word} is ${
-        fears[otherType]
-      } afraid of a ${otherType}`
-      room.assert(fearFact)
-      console.log(fearFact)
-    }
+  room.subscribe(`$name is a $type animal at ($x, $y)`, ({assertions, retractions}) => {
+    assertions.forEach(animal => {
+      let { type, name } = animal
+      if (animalsWeHaveSeen.has(name.word)) return
+      let fears = {
+        [type.word]: 'not',
+        mouse: 'mildly'
+      }
+      switch (type.word) {
+        case 'cat':
+          fears['dog'] = 'very'
+          fears['mouse'] = 'not'
+          break
+        case 'elephant':
+          fears['mouse'] = 'very'
+          break
+        case 'dog':
+          fears['cat'] = 'very'
+          break
+        case 'mouse':
+          fears['cat'] = 'very'
+          break
+        default:
+          break
+      }
+
+      for (var otherType in fears) {
+        let fearFact = `${type.word} ${name.word} is ${
+          fears[otherType]
+        } afraid of a ${otherType}`
+        room.assert(fearFact)
+        console.log(fearFact)
+      }
+      animalsWeHaveSeen.add(name.word)
+    })
   })
-})
+}
