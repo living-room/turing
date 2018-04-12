4db30e40c6207c3e1c62fa9c3d61aa43e3447805
Jonathan Dahan <hi@jonathan.is>
2018-04-12 16:31:30 -0400

    Update sightlines to work with processes

--- examples/processes/sightlines.js
+++ examples/processes/sightlines.js
@@ -1,29 +1,30 @@
-const Room = require('../build/room.js')
-const room = new Room() // grabs from process.env.LIVING_ROOM_HOST
-
-const lines = new Map()
-
-room.subscribe(
-  [
-    `$a sees $b`,
-    `$a is a $aspecies animal at ($ax, $ay)`,
-    `$b is a $bspecies animal at ($bx, $by)`
-  ],
-  seers => {
-    seers.forEach(({ a, b, ax, ay, bx, by }) => {
-      const id = a.word + b.word
-      const p = lines.get(id)
-      if (!p || p.ax != ax || p.ay != ay || p.bx != bx || p.by != by) {
-        lines.set(id, { ax, ay, bx, by })
-        const sightline = `${id}sightline is a (255, 127, 255) line from (${ax}, ${ay}) to (${bx}, ${by})`
-        console.log(sightline)
-        setTimeout(() => {
-          const previoussightline = `${id}sightline is a (255, 127, 255) line from (${ax}, ${ay}) to (${bx}, ${by})`
-          room.retract(previoussightline)
-        }, 100)
-
-        room.assert(sightline)
-      }
-    })
+module.exports = room => {
+  if (!room) {
+    const Room = require('../build/room.js')
+    const room = new Room()
   }
-)
+
+  const lines = new Map()
+
+  room.subscribe(
+    [
+      `$a sees $b`,
+      `$a is a $aspecies animal at ($ax, $ay)`,
+      `$b is a $bspecies animal at ($bx, $by)`
+    ],
+    async seers => {
+      seers.forEach(async ({ a, b, ax, ay, bx, by }) => {
+        const id = a.word + b.word
+        const p = lines.get(id)
+        if (!p || p.ax != ax || p.ay != ay || p.bx != bx || p.by != by) {
+          lines.set(id, { ax, ay, bx, by })
+
+          await room.assert(`${id}sightline is a (255, 127, 255) line from (${ax}, ${ay}) to (${bx}, ${by})`)
+
+          room.retract(`${id}sightline is a (255, 127, 255) line from (${p.ax}, ${p.ay}) to (${bx}, ${by})`)
+
+        }
+      })
+    }
+  )
+}
