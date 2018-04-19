62d92f3063a4b7d8ac88774aaeb7a270f44642de
Jonathan Dahan <hi@jonathan.is>
2018-04-19 11:12:11 -0400

    factor out sight distance variable

--- examples/processes/sight.js
+++ examples/processes/sight.js
@@ -1,21 +1,47 @@
+// $name is a $species animal at ($x, $y)
+// $species can see $distance
+
 module.exports = room => {
   if (!room) {
     const Room = require('../build/room.js')
     const room = new Room()
   }
 
+  // this only happens once on startup
+  async seed = () => {
+    // get all the species
+    const species = new Set(await room.select(`$ is a $species animal at ($, $)`)
+                              .map(({assertions}) => assertions)
+                              .map(({species}) => species.word))
+
+    // get all the species with sight
+    const sights = new Set(await room.select(`$species can see $`)
+                              .map(({assertions}) => assertions)
+                              .map(({species}) => species.word))
+
+    // if a specie does not have sight, give it sight
+    for (specie of species) {
+      if (!sights.has(specie)) {
+        room.assert(`${species} can see _`, Math.floor(5 * Math.random()))
+      }
+    }
+  }
+  seed()
+
   room.subscribe(
     [
       `$a is a $aspecies animal at ($ax, $ay)`,
-      `$b is a $bspecies animal at ($bx, $by)`
+      `$b is a $bspecies animal at ($bx, $by)`,
+      `$aspecies can see $distance`
     ],
+    // why is this async?
     async ({assertions, retractions}) => {
-      assertions.forEach(async ({ a, b, ax, ay, bx, by }) => {
+      assertions.forEach(async ({ a, b, ax, ay, bx, by, distance }) => {
         if (a.word === b.word) return
         const [dx, dy] = [100 * (ax.value - bx.value), 100 * (ay.value - by.value)]
         const seesFact = `${a.word} sees ${b.word}`
 
-        if ((dx * dx) + (dy * dy) < (5 * 5)) {
+        if ((dx * dx) + (dy * dy) < ($distance * $distance)) {
           room.assert(seesFact).then(console.dir)
         } else {
           room.retract(seesFact).then(console.dir)
