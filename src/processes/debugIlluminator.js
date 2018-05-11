a8b07d330ab2899021d15a2e34bda4749ce41dff
jedahan <hi@jonathan.is>
2018-05-11 14:18:40 -0400

    Add debug illuminator

--- src/processes/debugIlluminator.js
+++ src/processes/debugIlluminator.js
@@ -1,7 +1,62 @@
 // This labels displays
 
 module.exports = async room => {
-  room.assert(`table: draw label table at (0.1, 0.1)`)
-  room.assert(`whiteboard: draw label whiteboard at (0.1, 0.1)`)
-  room.assert(`draw label display at (0.1, 0.15)`)
+  const em = 0.05
+  const right_x = 1 - 5 * em
+  const activeProcesses = new Map()
+
+  const facts = [
+    `draw label display at (${em}, ${em})`,
+    `table: draw label table at (${em}, ${em + em})`,
+    `whiteboard: draw label whiteboard at (${em}, ${em + em})`,
+    `whiteboard: draw text "active processes" at (${right_x}, ${em})`
+  ]
+
+  room.subscribe(`$name is active`, ({ assertions, retractions }) => {
+    retractions.forEach(({ name }) => {
+      const index =
+        (activeProcesses.get(name.word) &&
+          activeProcesses.get(name.word).index) ||
+        activeProcesses.size + 1
+      activeProcesses.set(name.word, { index, active: false })
+    })
+
+    assertions.forEach(({ name }) => {
+      const index =
+        (activeProcesses.get(name.word) &&
+          activeProcesses.get(name.word).index) ||
+        activeProcesses.size + 1
+      activeProcesses.set(name.word, { index, active: true })
+    })
+
+    if (
+      activeProcesses.has('debugIlluminator') &&
+      !activeProcesses.get('debugIlluminator').active
+    ) {
+      facts.forEach(room.retract.bind(room))
+      Array.from(activeProcesses.entries()).forEach(([name, { index }]) => {
+        const y = (2 + index + 1) * em
+        const fact = `whiteboard: draw label ${name} at (${right_x}, ${y})`
+        room.retract(fact)
+      })
+    }
+
+    if (
+      activeProcesses.has('debugIlluminator') &&
+      activeProcesses.get('debugIlluminator').active
+    ) {
+      facts.forEach(room.assert.bind(room))
+      Array.from(activeProcesses.entries()).forEach(
+        ([name, { index, active }]) => {
+          const y = (2 + index + 1) * em
+          const fact = `whiteboard: draw label ${name} at (${right_x}, ${y})`
+          if (active) {
+            room.assert(fact)
+          } else {
+            room.retract(fact)
+          }
+        }
+      )
+    }
+  })
 }
