35964ac6ad14b046799edfd4d34a7daa3715fa5d
Jonathan Dahan <hi@jonathan.is>
2018-05-17 17:10:41 -0400

    add keyboard illuminator

---
+++ src/processes/keyboardIlluminator.js
@@ -0,0 +1,24 @@
+module.exports = async room => {
+  if (!room) {
+    const Room = require('@living-room/client-js')
+    room = new Room()
+  }
+
+  let buf = []
+
+  room.subscribe([
+    `keyboardIlluminator is active`,
+    `$mac got input event type $type with code $code and value $value`
+  ], ({ assertions }) => {
+    assertions.forEach(({code, type, value}) => {
+      if (code.value !== 4) return
+      if (type.value !== 4) return
+      console.log(buf.join(''))
+      room.retract(`whiteboard: draw small text "${buf.join(' ')}" at (0.05, 0.20)`)
+      buf.push(value.value - 458700)
+      room.assert(`whiteboard: draw small text "${buf.join(' ')}" at (0.05, 0.20)`)
+    })
+  })
+
+  room.assert(`keyboardIlluminator is active`)
+}
