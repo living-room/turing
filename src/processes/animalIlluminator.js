1f6bf68287065efaebd94b9f249bbc711d3d8eb6
Jonathan Dahan <hi@jonathan.is>
2018-05-20 13:35:18 -0400

    Remove unused code

--- src/processes/animalIlluminator.js
+++ src/processes/animalIlluminator.js
@@ -1,11 +1,14 @@
+// Draw animals on the table
 module.exports = room => {
   if (!room) {
     const Room = require('@living-room/client-js')
     room = new Room()
   }
 
-  room.on(`$name is a $ animal at ($x, $y) @ $t`, ({ name, x, y }) => {
-    room.retract(`table: draw label ${name} at ($, $)`)
-    room.assert(`table: draw label ${name} at (${x}, ${y})`)
-  })
+  room.on(
+    `$name is a $ animal at ($x, $y) @ $`,
+    ({ name, x, y }) => {
+      room.retract(`table: draw label ${name} at ($, $)`)
+      room.assert(`table: draw label ${name} at (${x}, ${y})`)
+    })
 }
