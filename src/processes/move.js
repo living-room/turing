f5dc6f34618089dd1e76c9dfa7382945db6063a5
Jonathan Dahan <hi@jonathan.is>
2018-05-22 11:35:54 -0400

    fix move by destructuring select

--- src/processes/move.js
+++ src/processes/move.js
@@ -5,15 +5,19 @@ module.exports = room => {
   }
 
   room.on(
-    `$name is a $species animal at ($x, $y) @ $p`,
-    `$name has speed ($dx, $dy)`,
-    `time is $t`,
-    ({ name, species, x, y, dx, dy, p, t }) => {
-      if (t <= p) return
-      room.retract(`${name} is a ${species} animal at (${x}, ${y}) @ ${p}`)
-      room.assert(
-        `${name} is a ${species} animal at (${x + dx}, ${y + dy}) @ ${t + 1}`
-      )
+    `$name is a $species animal at ($x, $y) @ $previous`,
+    `time is $current`,
+    `move is active`,
+    ({name, species, x, y, previous, current}) => {
+      if (current < previous) return
+      const speed = await room.select(`${name} has speed ($dx, $dy)`)
+      if (speed.length === 0) return
+      const {dx: {value: dx}, dy: {value: dy}} = speed[speed.length - 1]
+
+      room.retract(`${name} is a ${species} animal at (${x}, ${y}) @ ${previous}`)
+      room.assert(`${name} is a ${species} animal at (${x + dx}, ${y + dy}) @ ${current + 1}`)
     }
   )
+
+  room.assert('move is active')
 }
