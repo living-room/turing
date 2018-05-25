0c0b55592da76511ca4af8b1c177a884e99d183d
Jonathan Dahan <hi@jonathan.is>
2018-05-25 14:59:20 -0400

    Forward port 3000

--- server.js
+++ server.js
@@ -1,1 +1,8 @@
-require('@living-room/service-js/service.js')
+const service = require('@living-room/service-js')
+const forward = require('http-port-forward')
+
+const proxy = async () => {
+  const { port }= await service.listen()
+  forward(port, 3000)
+}
+proxy()
