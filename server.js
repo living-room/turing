74bcd2fe33eaff5754b45d72431a5dbda000c375
Jonathan Dahan <hi@jonathan.is>
2018-05-27 16:38:34 -0400

    remove unused packages

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const service = require('@living-room/service-js')
 const forward = require('http-port-forward')
 
 const proxy = async () => {
-  const { port }= await service.listen()
+  const { port } = await service.listen({ verbose: false })
   forward(port, 3000)
 }
 proxy()
