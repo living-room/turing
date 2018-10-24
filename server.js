e2dc81b812e08c9d77ac31412b02b2c77c3f972d
Jonathan Dahan <hi@jonathan.is>
2018-10-24 15:43:19 -0400

    opn is a promise

--- server.js
+++ server.js
@@ -31,6 +31,6 @@ service.listen({ verbose: false }).then(({ port, oscport }) => {
     }
     const message = color.red(`displays and tools at\n${color.white(uri)}`)
     console.log(boxen(message, formatting))
-    try { opn(uri) } catch { }
+    opn(uri).catch(console.log)
   })
 })
